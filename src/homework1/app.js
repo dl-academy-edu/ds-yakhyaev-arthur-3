62062e097df8e19242d7224e3120ce20e2a63993
Артур Яхьяев <arturahaev@MacBook-Air-Artur.local>
2021-05-03 22:11:24 +0400

    homework#1 (name,surname,age)

---
+++ src/homework1/app.js
@@ -0,0 +1,12 @@
+
+let name = prompt ('Введите ваше имя:')
+let surname = prompt ('Введите вашу фамилию:')
+let age = prompt ('Введите сколько вам лет:')
+
+user = {
+	name:name,
+	surname:surname,
+	age:age
+}
+
+console.log(user);
